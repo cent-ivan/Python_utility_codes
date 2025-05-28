cd037dbfb7a0ccd910119011c9e9b3ec00c25387
Vincent_Ivan <vpalomata25@gmail.com>
2025-05-29 05:12:11 +0800

    added alternative

--- checker/prime_number_checker.py
+++ checker/prime_number_checker.py
@@ -15,6 +15,23 @@ for base in range(2,length + 1):
     else:
         print(base,"is a prime number\n")
 
+#Alternative-----
+# def is_prime(number:int): 
+#     count = 0
+#     for base in range(1, number + 1):
+#         if number % base == 0:
+#             count += 1
+    
+#     if count > 2:
+#         pass
+#     else:
+#         print(f"{number} is a prime number")
+#         # print(f"{number} / {base} == {number / base}")
+
+# number = 110
+# for i in range(2, number  + 1):
+#     is_prime(i)
+
 #----------------------------------------------------
 end = time.perf_counter()
 elapse_time = end - start
