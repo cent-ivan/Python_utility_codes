3b65ab7a312308258470c1376a964935d5831390
Vincent_Ivan <vpalomata25@gmail.com>
2025-05-29 05:55:45 +0800

    **example

--- playground.py
+++ playground.py
@@ -3,8 +3,18 @@ import time
 
 start = time.perf_counter() #starts the performance timer
 #-Code here-------------------------------------------
+#Accepts keyword arguments, then you can use **.
+def my_function(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key} = {value}")
+        
+params = {
+    "name" : "Alice",
+    "age" : 30,
+    "country" : "USA"
+}
 
-
+my_function(**params)
 
 #----------------------------------------------------
 end = time.perf_counter()
